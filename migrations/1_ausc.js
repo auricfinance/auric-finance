057a08222ffa56254f2c4a1c1db01b106cdd5600
DVLB64 <dvlb64@protonmail.com>
2020-10-29 09:14:43 -0400

    Removed migrations to enable the tests

--- migrations/1_ausc.js
+++
@@ -1,9 +0,0 @@
-const AUSC = artifacts.require("AUSC");
-
-module.exports = async function(deployer, network, accounts) {
-  if (network === 'mainnet') {
-    await deployer.deploy(AUSC);
-    await AUSC.deployed();
-  }
-};
-
